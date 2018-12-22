d6f3d2cf4e591bcafad4af8e6746a65e25e51d9b
DenisBarzanov <denis.barzanov2002@gmail.com>
2018-12-22 22:07:24 +0000

    Added basic admin stuff with a material nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,59 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppRoutingModule} from './app-routing.module';
+import {CarsComponent} from './cars/cars.component';
+import {BookingsComponentComponent} from './bookings-component/bookings-component.component';
+import {BookingRequestsComponent} from './booking-requests/booking-requests.component';
+import {CarDetailComponent} from './car-detail/car-detail.component';
+import {MessagesComponent} from './messages/messages.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {HttpClientModule} from '@angular/common/http';
+import {InMemoryDataService} from './in-memory-data.service';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {AdminNavComponent} from './admin-nav/admin-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarsComponent,
+    BookingsComponentComponent,
+    BookingRequestsComponent,
+    CarDetailComponent,
+    MessagesComponent,
+    AdminNavComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+
+// The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+// and returns simulated server responses.
+// Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}),
+
+    LayoutModule,
+
+    MatToolbarModule,
+
+    MatButtonModule,
+
+    MatSidenavModule,
+
+    MatIconModule,
+
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
